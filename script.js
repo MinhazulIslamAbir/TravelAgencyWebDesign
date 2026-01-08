bf1caee65f04a6f04d4e56f92c698ed95a024284
unknown <minhazulislamabir5@gmail.com>
2026-01-08 16:29:42 +0600

    removal of unwanted file

--- script.js
+++
@@ -1,6 +0,0 @@
-let age;
-document.getElementById("SubmitButton").onclick = function(){
-    age = document.getElementById("Textbox").value;
-    if(age < 18) window.alert("You must be 18 years old or older.");
-    else window.alert("You can watch it.");
-}
--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
- n = numbs.length;
-    // for(let i = 0; i < n; i++){
-    //     [numbs[i], numbs[n - 1 - i]] = [numbs[n - 1 - i], numbs[i]];
-    // }
